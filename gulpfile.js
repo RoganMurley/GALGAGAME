37e420deb4892e0683ac0481f5b8498d392d5e8f
Rogan Murley <rogan@roganmurley.com>
2016-09-23 09:01:41 +0100

    Added basic room support

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,17 @@ var elm  = require('gulp-elm');
 gulp.task('elm-init', elm.init);
 
 gulp.task('elm', ['elm-init'], function(){
-  return gulp.src('src/*.elm')
+  return gulp.elm('elm/*.elm')
     .pipe(elm())
     .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('elm-bundle', ['elm-init'], function(){
-  return gulp.src('src/*.elm')
+  return gulp.elm('elm/*.elm')
     .pipe(elm.bundle('main.js'))
     .pipe(gulp.dest('static/'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('src/*.elm', ['elm-bundle']);
+  gulp.watch('elm/*.elm', ['elm-bundle']);
 });
