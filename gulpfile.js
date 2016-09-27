ae5e0c871566a83a2f76e2163beaa02da989b143
Rogan Murley <rogan@potatolondon.com>
2016-09-27 08:19:40 +0100

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,16 @@
 var gulp = require('gulp');
 var elm  = require('gulp-elm');
 
-gulp.task('elm-init', elm.init);
+gulp.task('init', elm.init);
 
-gulp.task('elm', ['elm-init'], function(){
-  return gulp.elm('elm/*.elm')
-    .pipe(elm())
-    .pipe(gulp.dest('dist/'));
-});
-
-gulp.task('elm-bundle', ['elm-init'], function(){
-  return gulp.elm('elm/*.elm')
-    .pipe(elm.bundle('main.js'))
+gulp.task('multi', ['init'], function(){
+  return gulp.src('elm/*.elm')
+    .pipe(elm.make({filetype: 'js'}))
     .pipe(gulp.dest('static/'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('elm/*.elm', ['elm-bundle']);
+  return gulp.watch('elm/*.elm', ['multi']);
 });
+
+gulp.task('default', ['watch']);
