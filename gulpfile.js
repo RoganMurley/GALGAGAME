332f0501c7eca40b85cfca3d6a264becef23ec1a
Rogan Murley <rogan@roganmurley.com>
2016-09-27 22:14:43 +0100

    Added other hand

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var elm  = require('gulp-elm');
+var plumber = require('gulp-plumber');
 
 gulp.task('init', elm.init);
 
 gulp.task('multi', ['init'], function(){
   return gulp.src('elm/*.elm')
+    .pipe(plumber())
     .pipe(elm.make({filetype: 'js'}))
     .pipe(gulp.dest('static/'));
 });
@@ -13,4 +15,4 @@ gulp.task('watch', function(){
   return gulp.watch('elm/*.elm', ['multi']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['multi', 'watch']);
