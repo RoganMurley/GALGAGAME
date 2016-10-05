5877f621a3b0620182eefb7c4379cc1f9fb2e3c8
Rogan Murley <rogan@potatolondon.com>
2016-10-05 08:57:20 +0100

    Made sync sync both hands

--- src/GameState.hs
+++ src/GameState.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module GameState where
 
+import Data.Aeson (ToJSON(..), (.=), object)
 import Data.Text (Text)
 
 
 type Card = Text
-type Model = [Card]
+type Hand = [Card]
+data Model = Model Hand Hand
+
+instance ToJSON Model where
+  toJSON (Model paHand pbHand) =
+    object ["paHand" .= paHand, "pbHand" .= pbHand]
 
 data GameCommand = Draw
 
@@ -14,4 +20,4 @@ update :: GameCommand -> Model -> Model
 update Draw model = drawCard model
 
 drawCard :: Model -> Model
-drawCard model = "blacklotus.jpg" : model
+drawCard (Model paHand pbHand) = Model ("blacklotus.jpg" : paHand) pbHand
