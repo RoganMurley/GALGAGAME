33ab4e274b83b61bfde9f7a1f4f0d5d208e4bd77
Rogan Murley <rogan@roganmurley.com>
2016-11-27 13:51:00 +0000

    Added basic test case stuff

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $
+  testGroup "Tests"
+    [ testGroup "Unit tests"
+      [ testCase "List comparison (different length)" $
+        [1, 2, 3] `compare` [1,2] @?= GT
+
+      , testCase "List comparison (same length)" $
+        [1, 2, 3] `compare` [1,2,2] @?= EQ
+      ]
+    ]
