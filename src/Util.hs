e48e1371c13fefad66fbc8ef24e20d697256eca2
Rogan Murley <rogan@potatolondon.com>
2016-12-07 14:15:24 +0000

    Better shuffling yay

--- src/Util.hs
+++ src/Util.hs
@@ -1,5 +1,12 @@
 module Util where
 
+import System.Random.Shuffle (shuffle')
+
+
 -- Apply a function n times.
 times :: Int -> (a -> a) -> a -> a
 times n f x = (iterate f x) !! n
+
+-- Convenient shuffle.
+shuffle :: [a] -> b
+shuffle xs = shuffle xs (length xs)
