d5d22da1eac972a0a2ebe63c794f7e9ffb5d9549
Rogan Murley <rogan@potatolondon.com>
2017-02-19 17:53:21 +0000

    Changed cards

--- src/Characters.hs
+++ src/Characters.hs
@@ -117,7 +117,7 @@ drinker :: Character
 drinker = Character "Drinker" (cardVampire, cardSuccubus, cardSiren, cardReversal)
 
 watcher :: Character
-watcher = Character "Watcher" (cardStaff, cardEnvy, cardZen, cardPrecognition)
+watcher = Character "Watcher" (cardStaff, cardEnvy, cardMindhack, cardPrecognition)
 
 imposter :: Character
-imposter = Character "Imposter" (cardShuriken, cardSuperego, cardEmpathy, cardFeint)
+imposter = Character "Imposter" (cardShuriken, cardSuperego, cardMindgate, cardFeint)
