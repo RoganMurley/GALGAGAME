18a1102d9fe5cef3683a487fca5e662811915b81
Rogan Murley <rogan@roganmurley.com>
2017-02-28 08:59:04 +0000

    Better audio API

---
+++ elm/Audio.elm
@@ -0,0 +1,48 @@
+module Audio exposing (..)
+
+import Ports exposing (playAudio)
+import Messages exposing (Msg)
+
+
+type SoundOption
+    = Once
+    | Loop
+    | Volume Float
+
+
+playSound : String -> Cmd Msg
+playSound name =
+    playSoundWith name []
+
+
+playSoundWith : String -> List SoundOption -> Cmd Msg
+playSoundWith name options =
+    let
+        once : Bool
+        once =
+            List.member Once options
+
+        loop : Bool
+        loop =
+            List.member Loop options
+
+        volume : Float
+        volume =
+            let
+                isVolOption : SoundOption -> Bool
+                isVolOption op =
+                    case op of
+                        Volume _ ->
+                            True
+
+                        otherwise ->
+                            False
+            in
+                case List.filter isVolOption options of
+                    [ Volume v ] ->
+                        v
+
+                    otherwise ->
+                        1.0
+    in
+        playAudio ( name, once, loop, volume )
