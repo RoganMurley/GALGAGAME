f02e8b9805e97df6c08a00df9213151fc2b37df6
Rogan Murley <rogan@roganmurley.com>
2017-04-09 17:35:37 +0100

    Cleaner and proper scaling :D

--- elm/Raymarch.elm
+++ elm/Raymarch.elm
@@ -37,7 +37,7 @@ view (Params theta ( w, h )) =
             theta / 1000
 
         downscale =
-            3
+            1
     in
         WebGL.toHtml
             [ width (w // downscale)
@@ -48,29 +48,20 @@ view (Params theta ( w, h )) =
                 vertexShader
                 fragmentShader
                 quadMesh
-                (uniforms time (Mat4.makeRotate 0 (Vec3.vec3 0 0 1.0)) (Vec3.vec3 1.0 1.0 0))
-            , WebGL.entityWith []
-                vertexShader
-                fragmentShader
-                quadMesh
-                (uniforms time (Mat4.makeRotate pi (Vec3.vec3 0 0 1.0)) (Vec3.vec3 1.0 1.0 0))
+                (uniforms time ( w, h ))
             ]
 
 
 type alias Uniforms =
-    { perspective : Mat4
-    , time : Float
-    , rotation : Mat4
-    , translation : Vec3
+    { time : Float
+    , resolution : Vec2
     }
 
 
-uniforms : Float -> Mat4.Mat4 -> Vec3 -> Uniforms
-uniforms theta rotation translation =
-    { perspective = Mat4.makeOrtho2D 0 1 0 1
-    , time = theta
-    , rotation = rotation
-    , translation = translation
+uniforms : Float -> ( Width, Height ) -> Uniforms
+uniforms theta ( width, height ) =
+    { time = theta
+    , resolution = vec2 (toFloat width) (toFloat height)
     }
 
 
@@ -80,7 +71,6 @@ uniforms theta rotation translation =
 
 type alias Vertex =
     { position : Vec3
-    , uv : Vec2
     }
 
 
@@ -88,16 +78,16 @@ quadMesh : Mesh Vertex
 quadMesh =
     let
         topRight =
-            { position = vec3 0.5 -0.5 0, uv = vec2 1 0 }
+            { position = vec3 1 -1 0 }
 
         bottomRight =
-            { position = vec3 0.5 0.5 0, uv = vec2 1 1 }
+            { position = vec3 1 1 0 }
 
         bottomLeft =
-            { position = vec3 -0.5 0.5 0, uv = vec2 0 1 }
+            { position = vec3 -1 1 0 }
 
         topLeft =
-            { position = vec3 -0.5 -0.5 0, uv = vec2 0 0 }
+            { position = vec3 -1 -1 0 }
     in
         WebGL.triangles
             [ ( topLeft, topRight, bottomLeft )
@@ -109,97 +99,89 @@ quadMesh =
 -- Shaders
 
 
-vertexShader : Shader Vertex Uniforms { vUv : Vec2 }
+vertexShader : Shader Vertex Uniforms {}
 vertexShader =
     [glsl|
         precision mediump float;
 
         attribute vec3 position;
-        attribute vec2 uv;
 
-        uniform mat4 perspective;
-        uniform mat4 rotation;
-        uniform vec3 translation;
         uniform float time;
 
-        varying vec2 vUv;
-
         void main () {
-            vUv = uv;
-            gl_Position = perspective * rotation * vec4(position, 1.0) + vec4(translation, 0);
+            gl_Position = vec4(position, 1.0);
         }
 
     |]
 
 
-fragmentShader : Shader {} Uniforms { vUv : Vec2 }
+fragmentShader : Shader {} Uniforms {}
 fragmentShader =
     [glsl|
         precision mediump float;
+
         uniform float time;
-        varying vec2 vUv;
+        uniform vec2 resolution;
 
         const int ITERATIONS = 64;
         const float Z_MAX = 40.0;
         const float EPSILON = 0.01;
-        const vec3 CAMERA_POS = vec3(0.0, 0.0, -5.0);
 
         const float RADIUS = 1.0;
 
         float map(vec3 pos)
         {
-            return length(pos) - RADIUS;
+          return length(pos) - RADIUS;
         }
 
         vec3 surface(vec3 pos)
         {
-            const vec2 eps = vec2(0.0, EPSILON);
+          const vec2 eps = vec2(0.0, EPSILON);
 
-            float ambientIntensity = 0.1;
-            vec3 lightDir = vec3(0.0, -0.5, 0.5);
+          float ambientIntensity = 0.1;
+          vec3 lightDir = vec3(0.0, -0.5, 0.5);
 
-            vec3 normal = normalize(vec3(
-                map(pos + eps.yxx) - map(pos - eps.yxx),
-                map(pos + eps.xyx) - map(pos - eps.xyx),
-                map(pos + eps.xxy) - map(pos - eps.xxy))
-            );
+          vec3 normal = normalize(vec3(
+              map(pos + eps.yxx) - map(pos - eps.yxx),
+              map(pos + eps.xyx) - map(pos - eps.xyx),
+              map(pos + eps.xxy) - map(pos - eps.xxy))
+          );
 
-            float diffuse = ambientIntensity + max(dot(-lightDir, normal), 0.0);
+          float diffuse = ambientIntensity + max(dot(-lightDir, normal), 0.0);
 
-            return vec3(diffuse, diffuse, diffuse);
+          return vec3(diffuse, diffuse, diffuse);
         }
 
         void main ()
         {
-            vec2 uv = vUv - 0.5;
-            vec3 camUp = vec3(0.0, 1.0, 0.0);
-            vec3 camForward = vec3(0.0, 0.0, 1.0);
-            vec3 camRight = vec3(1.0, 0.0, 0.0);
+          vec2 uv = gl_FragCoord.xy / resolution.xy;
+          uv = uv * 2.0 - 1.0;
+          uv.x *= resolution.x / resolution.y;
 
-            vec3 ray = camUp * uv.y + camRight * uv.x + camForward;
-            vec3 color = vec3(0.0, 0.0, 0.0);
-            vec3 pos = CAMERA_POS;
+          vec3 ray = normalize(vec3(uv, 1.0));;
+          vec3 color = vec3(0.0, 0.0, 0.0);
+          vec3 pos = vec3(0.0, 0.0, -3.0);
 
-            // Raymarching
-            for (int i = 0; i < ITERATIONS; ++i)
-            {
-                float distance = map(pos);
+          // Raymarching
+          for (int i = 0; i < ITERATIONS; ++i)
+          {
+              float distance = map(pos);
 
-                if (distance < EPSILON)
-                {
-                    color = surface(pos);
-                    break;
-                }
+              if (distance < EPSILON)
+              {
+                  color = surface(pos);
+                  break;
+              }
 
-                pos += ray * distance;
+              pos += ray * distance;
 
-                if (distance > Z_MAX)
-                {
-                    break;
-                }
-            }
+              if (distance > Z_MAX)
+              {
+                  break;
+              }
+          }
 
-            gl_FragColor = vec4(color, 1.0);
+          gl_FragColor = vec4(color, 1.0);
         }
 
     |]
