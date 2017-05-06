6b97557b3b8662b66c7348b20c0a6b0db31da463
Rogan Murley <rogan@roganmurley.com>
2017-05-06 19:37:35 +0100

    Silly little changes

--- src/Util.hs
+++ src/Util.hs
@@ -36,19 +36,24 @@ fromRight _ = error "Unsafe fromRight unwrapping failure"
 -- Special newtype-wrapped StdGen to ease equality checks.
 newtype Gen = Gen R.StdGen
 
+
 instance Eq Gen where
   _ == _ = True
 
+
 instance Show Gen where
   show _ = "gen"
+
 
 split :: Gen -> (Gen, Gen)
 split (Gen g) = (Gen g1, Gen g2)
   where
     (g1, g2) = R.split g :: (R.StdGen, R.StdGen)
 
+
 getGen :: IO Gen
-getGen = fmap Gen R.getStdGen
+getGen = Gen <$> R.getStdGen
+
 
 mkGen :: Int -> Gen
 mkGen n = Gen . R.mkStdGen $ n
