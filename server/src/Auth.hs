8c2ca04cbecdedeaece4ed623bcf19be0982ffda
Rogan Murley <rogan@potatolondon.com>
2017-05-27 21:41:24 +0100

    Very stupid login and logout

--- server/src/Auth.hs
+++ server/src/Auth.hs
@@ -1,21 +1,31 @@
 module Auth where
 
 import Data.Monoid (mconcat)
+import Data.String.Conversions (cs)
 import Data.Text.Internal.Lazy (Text)
 import Network.Wai (Application)
 import Web.Scotty
+import Web.Scotty.Cookie (deleteCookie, setSimpleCookie)
 
 
 app :: IO Application
 app = scottyApp $ do
+  get "/login" $ do
+    html $
+      "<form action=\"login\" method=POST><input type=\"text\" name=\"username\"><input type=\"password\" name=\"password\"><input type=\"submit\" value=\"Login\"></form><br><form action=\"logout\" method=POST><input type=\"submit\" value=\"Logout\"></form>"
   post "/login" $ do
     username <- param "username"
     password <- param "password"
     case login username password of
-      True ->
+      True -> do
+        setSimpleCookie "login" (cs username)
         html $ mconcat ["<h1>Welcome, ", username, "!</h1>"]
       False ->
         html "Incorrect password."
+  post "/logout" $ do
+    deleteCookie "login"
+    html "Logged out."
+
 
 login :: Text -> Text -> Bool
 login username password =
