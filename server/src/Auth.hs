b4034720767b90151df8d9e2df198adececef756
Rogan Murley <rogan@roganmurley.com>
2017-05-28 20:09:15 +0100

    Stupid setup for login

--- server/src/Auth.hs
+++ server/src/Auth.hs
@@ -2,37 +2,44 @@ module Auth where
 
 import Control.Monad.Trans.Class (lift)
 import Data.Monoid (mconcat)
+import Data.String.Conversions (cs)
 import Data.Text.Internal.Lazy (Text)
 import Network.Wai (Application)
 import Web.Scotty
 import Web.Scotty.Cookie (deleteCookie, setSimpleCookie)
 
 import qualified Data.GUID as GUID
+import qualified Database.Redis as R
 
 
 app :: IO Application
-app = scottyApp $ do
-  get "/login" $ do
-    html $
-      "<form action=\"login\" method=POST><input type=\"text\" name=\"username\"><input type=\"password\" name=\"password\"><input type=\"submit\" value=\"Login\"></form><br><form action=\"logout\" method=POST><input type=\"submit\" value=\"Logout\"></form>"
-  post "/login" $ do
-    username <- param "username"
-    password <- param "password"
-    case login username password of
-      True -> do
-        token <- lift GUID.genText
-        setSimpleCookie "login" token
-        html $ mconcat ["<h1>Welcome, ", username, "!</h1>"]
-      False ->
-        html "Incorrect password."
-  post "/logout" $ do
-    deleteCookie "login"
-    html "Logged out."
+app = do
+  conn <- R.connect $ R.defaultConnectInfo { R.connectHost = "redis" }
+  scottyApp $ do
+    get "/login" $ do
+      html $
+        "<form action=\"login\" method=POST><input type=\"text\" name=\"username\"><input type=\"password\" name=\"password\"><input type=\"submit\" value=\"Login\"></form><br><form action=\"logout\" method=POST><input type=\"submit\" value=\"Logout\"></form><br><form action=\"register\" method=POST><input type=\"text\" name=\"username\"><input type=\"password\" name=\"password\"><input type=\"submit\" value=\"Register\"></form>"
+    post "/login" $ do
+      (username :: Text) <- param "username"
+      (password :: Text) <- param "password"
+      storedPassword <- lift . (R.runRedis conn) $ R.get (cs username)
+      case storedPassword of
+        Right (Just toad) ->
+          case (cs toad) == password of
+            True -> do
+              token <- lift GUID.genText
+              setSimpleCookie "login" token
+              html $ mconcat ["<h1>Welcome, ", username, "!</h1>"]
+            False ->
+              html "Incorrect password."
+        _ ->
+          html "redis lol"
 
-
-login :: Text -> Text -> Bool
-login username password =
-    elem (username, password) users
-  where
-    users :: [(Text, Text)]
-    users = [("master", "sword")]
+    post "/logout" $ do
+      deleteCookie "login"
+      html "Logged out."
+    post "/register" $ do
+      username <- param "username"
+      password <- param "password"
+      _ <- lift . (R.runRedis conn) $ R.set username password
+      html "Registered."
