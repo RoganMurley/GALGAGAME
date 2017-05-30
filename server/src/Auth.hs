a7a102017a2548965fe22660cbc4a883419468c7
Rogan Murley <rogan@potatolondon.com>
2017-05-30 19:57:41 +0100

    Cleaning up scotty

--- server/src/Auth.hs
+++ server/src/Auth.hs
@@ -2,8 +2,8 @@ module Auth where
 
 import Control.Monad.Trans.Class (lift)
 import Crypto.BCrypt (validatePassword, hashPasswordUsingPolicy, slowerBcryptHashingPolicy)
-import Data.Monoid (mconcat)
 import Data.String.Conversions (cs)
+import Network.HTTP.Types.Status
 import Network.Wai (Application)
 import Web.Scotty
 import Web.Scotty.Cookie (deleteCookie, getCookie, setSimpleCookie)
@@ -19,55 +19,60 @@ import qualified Data.Text.Encoding as T
 app :: R.Connection -> R.Connection -> IO Application
 app userConn tokenConn =
   scottyApp $ do
+    post "/login"    $ login userConn tokenConn
+    post "/logout"   $ logout tokenConn
+    post "/register" $ register userConn
 
-    get "/login" $ do
-      html $
-        "<form action=\"login\" method=POST><input type=\"text\" name=\"username\"><input type=\"password\" name=\"password\"><input type=\"submit\" value=\"Login\"></form><br><form action=\"logout\" method=POST><input type=\"submit\" value=\"Logout\"></form><br><form action=\"register\" method=POST><input type=\"text\" name=\"username\"><input type=\"password\" name=\"password\"><input type=\"submit\" value=\"Register\"></form>"
 
-    post "/login" $ do
-      username <- param "username"
-      password <- param "password"
-      gotten <- lift . (R.runRedis userConn) $ R.get username
-      case gotten of
-        Right pass ->
-          case pass of
-            Just hashedPassword ->
-              if validatePassword hashedPassword password
-                then ( do
-                  token <- lift GUID.genText
-                  setSimpleCookie "login" token
-                  _ <- lift . (R.runRedis tokenConn) $ do
-                    _ <- R.set (cs token) username
-                    R.expire (cs token) 60
-                  html $ mconcat ["<h1>Welcome, ", cs username, "!</h1>"]
-                )
-                else (html "Incorrect username or password.")
-            Nothing ->
-              html "Incorrect username or password."
-        Left _ -> do
-          lift . T.putStrLn $ "Database connection error"
-          html "Database connection error"
-
-    post "/logout" $ do
-      token <- getCookie "login"
-      case token of
-        Just t -> do
-          _ <- lift . (R.runRedis tokenConn) $ R.del [T.encodeUtf8 t]
-          deleteCookie "login"
-          html "Logged out."
+login :: R.Connection -> R.Connection -> ActionM ()
+login userConn tokenConn = do
+  username <- param "username"
+  password <- param "password"
+  gotten <- lift . (R.runRedis userConn) $ R.get username
+  case gotten of
+    Right pass ->
+      case pass of
+        Just hashedPassword ->
+          case validatePassword hashedPassword password of
+            True -> do
+              token <- lift GUID.genText
+              setSimpleCookie "login" token
+              _ <- lift . (R.runRedis tokenConn) $ do
+                _ <- R.set (cs token) username
+                R.expire (cs token) 60
+              status ok200
+            False -> do
+              status unauthorized401
         Nothing ->
-          html "Can't log out; not logged in"
+          status unauthorized401
+    Left _ -> do
+      lift . T.putStrLn $ "Database connection error"
+      status internalServerError500
 
-    post "/register" $ do
-      username <- param "username"
-      password <- param "password"
-      p <- lift $ hashPasswordUsingPolicy slowerBcryptHashingPolicy password
-      case p of
-        Just hashedPassword -> do
-          _ <- lift . (R.runRedis userConn) $ R.set username hashedPassword
-          html "Registered."
-        Nothing ->
-          html "Something went wrong with your registration."
+
+logout :: R.Connection -> ActionM ()
+logout tokenConn = do
+  token <- getCookie "login"
+  case token of
+    Just t -> do
+      _ <- lift . (R.runRedis tokenConn) $ R.del [T.encodeUtf8 t]
+      deleteCookie "login"
+      status noContent204
+    Nothing ->
+      status noContent204
+
+
+register :: R.Connection -> ActionM ()
+register userConn = do
+  username <- param "username"
+  password <- param "password"
+  p <- lift $ hashPasswordUsingPolicy slowerBcryptHashingPolicy password
+  case p of
+    Just hashedPassword -> do
+      _ <- lift . (R.runRedis userConn) $ R.set username hashedPassword
+      status created201
+    Nothing ->
+      status internalServerError500
 
 
 checkAuth :: R.Connection -> Token -> IO (Maybe Username)
