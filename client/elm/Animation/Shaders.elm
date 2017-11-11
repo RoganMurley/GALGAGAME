52bd1c7eda21ca6b46e7bc3f7486aa48d54d8466
Rogan Murley <rogan@roganmurley.com>
2017-11-11 22:07:25 +0000

    Animation tweaks

--- client/elm/Animation/Shaders.elm
+++ client/elm/Animation/Shaders.elm
@@ -33,12 +33,15 @@ fragmentShader =
             vec2 uv = gl_FragCoord.xy / resolution.xy;
 
             float radius = 0.3;
-            float progress = min(time, .8);
+            if (time > .8) {
+                discard;
+                return;
+            }
 
             gl_FragColor = vec4(1., 1., 1., 0.);
             if ((uv.x - .5) * (uv.x - .5) + (uv.y - .5) * (uv.y - .5) < radius * radius) {
-                if ((uv.x - .4) * (uv.x - .5) + (uv.y - .4) * (uv.y - .4) > radius * radius * (1.8 - .6 * abs(sin(4. * progress)))) {
-                    if (uv.x < abs(sin(progress) * 3.)) {
+                if ((uv.x - .4) * (uv.x - .5) + (uv.y - .4) * (uv.y - .4) > radius * radius * (1.8 - .6 * abs(sin(4. * time)))) {
+                    if (uv.x < abs(sin(time) * 3.)) {
                         gl_FragColor = vec4(1., .02, .02, 1.);
                         return;
                     }
