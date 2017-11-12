dc2b0e3d1e9d09e40e9d4de7301f3598daf0b1f1
Rogan Murley <rogan@roganmurley.com>
2017-11-12 14:18:44 +0000

    Improved lab

--- client/elm/Lab/View.elm
+++ client/elm/Lab/View.elm
@@ -7,8 +7,37 @@ import Lab.Messages exposing (Msg(..))
 import Lab.Types exposing (Model)
 import Raymarch.Types exposing (Params(..))
 import Animation.View as Animation
+import Card.Types exposing (Anim(..))
+import Model.Types exposing (WhichPlayer(..))
 
 
 view : Params -> Model -> Html Msg
 view params { player, anim, time } =
-    Animation.view params time (Just ( player, anim ))
+    div [ class "lab" ]
+        [ div [ class "sets" ]
+            [ fieldset []
+                [ legend [] [ text "Animation:" ]
+                , radio (SetAnim Slash) "anim" "Slash"
+                , radio (SetAnim Obliterate) "anim" "Obliterate"
+                ]
+            , fieldset []
+                [ legend [] [ text "Owner:" ]
+                , radio (SetPlayer PlayerA) "which-player" "Player A"
+                , radio (SetPlayer PlayerB) "which-player" "Player B"
+                ]
+            ]
+        , Animation.view params time (Just ( player, anim ))
+        ]
+
+
+radio : msg -> String -> String -> Html msg
+radio msg fGroup value =
+    label []
+        [ input
+            [ type_ "radio"
+            , onClick msg
+            , name fGroup
+            ]
+            []
+        , text value
+        ]
