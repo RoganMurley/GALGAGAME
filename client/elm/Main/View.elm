7d00cc04c91bb04cc568920718cb85d2b9c6e7bd
Rogan Murley <rogan@roganmurley.com>
2017-11-20 15:31:48 +0000

    Changed how flags are stored and used

--- client/elm/Main/View.elm
+++ client/elm/Main/View.elm
@@ -4,17 +4,20 @@ import Connected.View as Connected
 import Html as Html exposing (Html, div, text)
 import Lab.View as Lab
 import Lobby.View as Lobby
-import Main.Types as Main exposing (..)
+import Main.Types exposing (..)
 import Main.Messages exposing (Msg(..))
 import Menu.View as Menu
 import Raymarch.Types as Raymarch
 
 
-view : Main.Model -> Html Msg
-view ({ hostname, httpPort, frameTime, windowDimensions } as model) =
+view : Model -> Html Msg
+view model =
     let
+        { hostname, httpPort, time, windowDimensions } =
+            model.flags
+
         params =
-            Raymarch.Params frameTime windowDimensions
+            Raymarch.Params time windowDimensions
     in
         case model.room of
             MainMenu ->
