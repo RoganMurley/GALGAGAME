973e319493a7421d0bc9084d45de22dc2c6d4ac2
Rogan Murley <rogan@roganmurley.com>
2018-01-10 22:54:47 +0000

    Various free monad interpreter experiments

--- server/src/Model.hs
+++ server/src/Model.hs
@@ -149,7 +149,8 @@ data Alpha n =
 
 data Beta n
   = BetaRaw (AlphaProgram ()) n
-  | BetaDraw WhichPlayer n
+  | BetaSlash Life WhichPlayer n
+  | BetaGetLife WhichPlayer (Life -> n)
   deriving (Functor)
 
 
@@ -337,7 +338,7 @@ effI m (Free (SetTurn t n))   = effI (m { model_turn = t }) n
 effI m (Pure x)               = (m, x)
 
 
-modI :: Model -> AlphaProgram a -> Model
+modI :: Model -> AlphaProgram () -> Model
 modI m p = fst $ effI m p
 
 
@@ -346,11 +347,28 @@ evalI m p = snd $ effI m p
 
 
 alphaI :: BetaProgram a -> AlphaProgram a
-alphaI (Free (BetaRaw p n))  = p      >> alphaI n
-alphaI (Free (BetaDraw w n)) = draw w >> alphaI n
-alphaI (Pure x)              = Pure x
+alphaI (Free (BetaRaw p n))     = p         >>  alphaI n
+alphaI (Free (BetaSlash d w n)) = hurt d w  >>  alphaI n
+alphaI (Free (BetaGetLife w f)) = getLife w >>= (\l -> alphaI (f l))
+alphaI (Pure x)                 = Pure x
 
 
+-- Anim DSL
+data AnimDSL a
+  = AnimSlash a
+  deriving (Functor)
+
+
+type AnimProgram a = Free AnimDSL a
+
+
+animI :: Beta a -> AnimProgram ()
+animI (BetaRaw _ _)     = Pure ()
+animI (BetaSlash _ _ _) = liftF $ AnimSlash ()
+animI (BetaGetLife _ _) = Pure ()
+
+
+-- Log DSL
 data LogDSL a
   = Log String a
   deriving (Functor)
@@ -363,13 +381,13 @@ makeFree ''LogDSL
 
 
 logI :: Alpha a -> LogProgram ()
-logI (GetGen _)      = log $ "Get gen"
-logI (GetDeck _ _)   = log $ "Get deck"
-logI (GetHand _ _)   = log $ "Get hand"
-logI (GetLife _ _)   = log $ "Get life"
-logI (GetPasses _)   = log $ "Get passes"
-logI (GetStack _)    = log $ "Get stack"
-logI (GetTurn _)     = log $ "Get turn"
+logI (GetGen _)      = log $        "Get gen"
+logI (GetDeck w _)   = log $ printf "Get deck %s" (show w)
+logI (GetHand w _)   = log $ printf "Get hand %s" (show w)
+logI (GetLife w _)   = log $ printf "Get life %s" (show w)
+logI (GetPasses _)   = log $        "Get passes"
+logI (GetStack _)    = log $        "Get stack"
+logI (GetTurn _)     = log $        "Get turn"
 logI (SetGen g _)    = log $ printf "Set gen %s"     (show g)
 logI (SetDeck w d _) = log $ printf "Set deck %s %s" (show w) (show d)
 logI (SetHand w h _) = log $ printf "Set hand %s %s" (show w) (show h)
@@ -419,12 +437,12 @@ execute = execute' ""
 
 
 toLeft :: (Functor f, Functor g) => Free f a -> Free (Sum f g) a
-toLeft (Free f) = Free $ InL (toLeft <$> f)
+toLeft (Free f) = Free $ toLeft <$> InL f
 toLeft (Pure x) = Pure x
 
 
 toRight :: (Functor f, Functor g) => Free g a -> Free (Sum f g) a
-toRight (Free f) = Free $ InR (toRight <$> f)
+toRight (Free f) = Free $ toRight <$> InR f
 toRight (Pure x) = Pure x
 
 
@@ -434,3 +452,62 @@ prog = foldFree superI $ do
   life <- getLife PlayerA
   when (life < 10) $ setLife PlayerA 999
   getLife PlayerA
+
+
+hyperI :: Beta a -> Free (Sum Alpha AnimDSL) a
+hyperI op = toRight (animI op) *> toLeft (alphaI $ liftF op)
+
+
+exec :: Model -> Free (Sum Alpha AnimDSL) a -> (Model, a, String)
+exec = execute' ""
+  where
+    execute' :: String -> Model -> Free (Sum Alpha AnimDSL) a -> (Model, a, String)
+    execute' s m (Free (InL p))  =
+      let
+        (model, program) = progI m p
+      in
+                                          execute' s model program
+    execute' s m (Free (InR (AnimSlash n))) =
+                                          execute' (s ++ "slash\n") m n
+    execute' s m (Pure x) =
+                                          (m, x, s)
+
+
+bar ::  Free (Sum Alpha AnimDSL) Life
+bar = foldFree hyperI $ do
+  betaSlash 8 PlayerA
+  l <- betaGetLife PlayerA
+  when (l < 30) $ betaSlash 1 PlayerB
+  return l
+
+
+type FullProgram = Free (Sum AnimDSL (Sum Alpha LogDSL))
+
+
+ultraI :: ∀ a . Beta a -> FullProgram a
+ultraI op = toRight (foldFree superI (alphaI $ liftF op)) <* toLeft (animI op)
+
+
+baz ::  BetaProgram Life
+baz = do
+  betaSlash 8 PlayerA
+  l <- betaGetLife PlayerA
+  when (l < 45) $ betaSlash 1 PlayerB
+  return l
+
+
+ultraExec :: Model -> FullProgram a -> (Model, a, String, String)
+ultraExec = execute' "" ""
+  where
+    execute' :: String -> String -> Model -> FullProgram a -> (Model, a, String, String)
+    execute' s a m (Free (InL (AnimSlash n))) =
+                                          execute' s (a ++ "slash\n") m n
+    execute' s a m (Free (InR (InL p)))  =
+      let
+        (model, program) = progI m p
+      in
+                                          execute' s a model program
+    execute' s a m (Free (InR (InR (Log l n)))) =
+                                          execute' (s ++ l ++ "\n") a m n
+    execute' s a m (Pure x) =
+                                          (m, x, s, a)
