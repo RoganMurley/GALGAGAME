14e67189d7ac779f1a0c1d3a0848b661fc2e1e70
Rogan Murley <rogan@roganmurley.com>
2018-02-04 17:41:25 +0000

    Moved Transform to its own module

---
+++ client/elm/Transform.elm
@@ -0,0 +1,39 @@
+module Transform exposing (..)
+
+import Ease
+
+
+type alias Transform =
+    { x : Float
+    , y : Float
+    , r : Float
+    }
+
+
+ease : Ease.Easing -> Float -> Transform -> Transform -> Transform
+ease easing t start final =
+    let
+        diff : Transform
+        diff =
+            { x = final.x - start.x
+            , y = final.y - start.y
+            , r = final.r - start.r
+            }
+    in
+        { x = start.x + (easing t) * diff.x
+        , y = start.y + (easing t) * diff.y
+        , r = start.r + (easing t) * diff.r
+        }
+
+
+toCss : Transform -> ( String, String )
+toCss { x, y, r } =
+    ( "transform"
+    , "translate("
+        ++ toString x
+        ++ "rem, "
+        ++ toString y
+        ++ "rem) rotate("
+        ++ toString r
+        ++ "deg)"
+    )
