2fd0333cb761d7ca69c558d7299076e742edd562
Rogan Murley <rogan@roganmurley.com>
2018-02-04 17:38:12 +0000

    Move Stack view to its own module

--- client/elm/Room/Connected/GameState/Model/View.elm
+++ client/elm/Room/Connected/GameState/Model/View.elm
@@ -1,6 +1,5 @@
 module Model.View exposing (..)
 
-import Card.View as Card
 import Connected.Messages as Connected
 import Hand.State exposing (maxHandLength)
 import Html exposing (..)
@@ -14,6 +13,7 @@ import GameState.Messages exposing (..)
 import Model.Types exposing (..)
 import Model.ViewModel exposing (..)
 import Stack.Types exposing (..)
+import Stack.View as Stack
 import WhichPlayer.Types exposing (WhichPlayer(..))
 
 
@@ -31,7 +31,7 @@ view ( model, viewModel ) time =
         [ viewOtherHand model.otherHand model.otherHover 0 Nothing
         , Html.map playingOnly <|
             viewHand model.hand viewModel.hover 0 False Nothing
-        , viewStack model.stack
+        , Stack.view model.stack
         , Html.map playingOnly <|
             viewTurn (List.length model.hand == maxHandLength) model.turn
         , viewStatus PlayerA model.life
@@ -93,75 +93,6 @@ viewLife life =
             ]
 
 
-viewStack : Stack -> Html msg
-viewStack stack =
-    let
-        viewStackCard : ( Int, StackCard ) -> Html msg
-        viewStackCard ( index, { owner, card } ) =
-            let
-                cardWidth : Float
-                cardWidth =
-                    14.0
-
-                playerClass : String
-                playerClass =
-                    case owner of
-                        PlayerA ->
-                            "playera"
-
-                        PlayerB ->
-                            "playerb"
-
-                stackLen : Float
-                stackLen =
-                    toFloat (List.length stack)
-
-                offset : Int -> Float
-                offset x =
-                    cardWidth * (toFloat x)
-
-                squish : Float -> Float
-                squish x =
-                    Basics.min 0.0 (15.0 - 0.65 * (x + 1.0) * stackLen)
-
-                totalOffset : Float
-                totalOffset =
-                    (offset index)
-                        - (cardWidth * stackLen * 0.5)
-                        + (squish (toFloat index))
-                        - ((squish (stackLen - 1.0)) * 0.5)
-
-                rot : Float
-                rot =
-                    0.1 * (toFloat ((index * 1247823748932 + 142131) % 20) - 10)
-
-                headClass : String
-                headClass =
-                    case index of
-                        0 ->
-                            " stack-head"
-
-                        otherwise ->
-                            ""
-            in
-                div
-                    [ class (playerClass ++ " stack-card")
-                    , style
-                        [ ( "transform", "translateX(" ++ (toString totalOffset) ++ "rem)" )
-                        , ( "z-index", toString (20 - index) )
-                        ]
-                    ]
-                    [ div
-                        [ style [ ( "transform", "rotate(" ++ (toString rot) ++ "deg)" ) ] ]
-                        [ div [ class headClass ] [ Card.view card ]
-                        ]
-                    ]
-    in
-        div
-            [ class "stack-container" ]
-            [ div [ class "stack" ] (List.map viewStackCard (List.indexedMap (,) stack)) ]
-
-
 
 -- RESOLVING VIEW.
 
@@ -184,7 +115,7 @@ resView vm { model, stackCard, anim } time resTick =
             [ viewOtherHand model.otherHand model.otherHover resTick anim
             , Html.map playingOnly <|
                 viewHand model.hand vm.hover resTick True anim
-            , viewStack stack
+            , Stack.view stack
             , viewResTurn stackCard
             , viewStatus PlayerA model.life
             , viewStatus PlayerB model.otherLife
