1deee6540acabf236deb4762ec2015be92710fdb
Rogan Murley <rogan@roganmurley.com>
2018-04-18 17:53:09 +0100

    Add clock shape to WebGL demo

--- client/elm/Clock/State.elm
+++ client/elm/Clock/State.elm
@@ -1,9 +1,9 @@
 module Clock.State exposing (..)
 
 import Clock.Types exposing (Model, Uniforms)
-import Math.Matrix4 exposing (makeLookAt, makePerspective, makeRotate, mul)
+import Math.Matrix4 exposing (Mat4, makeLookAt, makePerspective, makeRotate, mul)
 import Math.Vector2 exposing (vec2)
-import Math.Vector3 exposing (vec3)
+import Math.Vector3 exposing (Vec3, vec3)
 import Raymarch.Types exposing (Height, Width)
 import WebGL exposing (Texture)
 
@@ -28,3 +28,39 @@ uniforms t ( width, height ) texture =
     , perspective = makePerspective 45 1 0.01 100
     , camera = makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
     }
+
+
+cameraUniforms : Vec3 -> { camera : Mat4, perspective : Mat4, rotation : Mat4, color : Vec3 }
+cameraUniforms color =
+    let
+        t =
+            0
+    in
+        { rotation = mul (makeRotate (3 * t) (vec3 0 1 0)) (makeRotate (2 * t) (vec3 1 0 0))
+        , perspective = makePerspective 45 1 0.01 100
+        , camera = makeLookAt (vec3 0 0 5) (vec3 0 0 0) (vec3 0 1 0)
+        , color = color
+        }
+
+
+clockFace : Int -> Vec3 -> Float -> List Vec3
+clockFace n origin radius =
+    let
+        indexes : List Int
+        indexes =
+            List.range 0 (n - 1)
+
+        genPoint : Int -> Vec3
+        genPoint i =
+            Math.Vector3.add origin (offset i)
+
+        offset : Int -> Vec3
+        offset i =
+            let
+                rot =
+                    (toFloat i) * 2.0 * pi / (toFloat n)
+            in
+                Math.Vector3.scale radius <|
+                    vec3 (sin rot) (cos rot) 0
+    in
+        List.map genPoint indexes
--- client/elm/Clock/View.elm
+++ client/elm/Clock/View.elm
@@ -4,6 +4,7 @@ import Clock.Types exposing (Model)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Main.Messages as Main
+import Math.Vector3 exposing (vec3)
 import Clock.Meshes
 import Clock.Shaders
 import Clock.State exposing (uniforms)
@@ -25,23 +26,63 @@ view (Params _ ( w, h )) { time } textures =
 
         mTexture =
             Texture.load textures "clock"
+
+        positions =
+            Clock.State.clockFace 12 (vec3 0 0 0) 1.0
     in
         div [ class "clock" ]
             (case mTexture of
                 Just texture ->
-                    [ WebGL.toHtml
-                        [ width (w // downscale)
-                        , height (h // downscale)
-                        , class "raymarch-canvas"
+                    let
+                        makeEntity pos =
+                            WebGL.entityWith
+                                [ WebGL.add WebGL.srcAlpha WebGL.oneMinusSrcAlpha ]
+                                Clock.Shaders.vertex
+                                Clock.Shaders.fragment
+                                (Clock.Meshes.quad pos 0.2)
+                                (uniforms theta ( w, h ) texture)
+
+                        entities =
+                            List.map makeEntity positions
+                    in
+                        [ WebGL.toHtml
+                            [ width (w // downscale)
+                            , height (h // downscale)
+                            , class "raymarch-canvas"
+                            ]
+                            entities
+                          -- [ WebGL.entityWith
+                          --     [ WebGL.add WebGL.srcAlpha WebGL.oneMinusSrcAlpha ]
+                          --     Clock.Shaders.vertex
+                          --     Clock.Shaders.fragment
+                          --     (Clock.Meshes.quad (vec3 -0.5 -0.5 0) 0.2)
+                          --     (uniforms theta ( w, h ) texture)
+                          -- , WebGL.entityWith
+                          --     [ WebGL.add WebGL.srcAlpha WebGL.oneMinusSrcAlpha ]
+                          --     Clock.Shaders.vertex
+                          --     Clock.Shaders.fragment
+                          --     (Clock.Meshes.quad (vec3 0.5 0.5 -0.3) 0.3)
+                          --     (uniforms theta ( w, h ) texture)
+                          -- , WebGL.entityWith
+                          --     [ WebGL.add WebGL.srcAlpha WebGL.oneMinusSrcAlpha ]
+                          --     Clock.Shaders.vertex
+                          --     Clock.Shaders.fragment
+                          --     (Clock.Meshes.quad (vec3 -0.5 0.5 0.7) 0.1)
+                          --     (uniforms theta ( w, h ) texture)
+                          -- , WebGL.entityWith
+                          --     [ WebGL.add WebGL.srcAlpha WebGL.oneMinusSrcAlpha ]
+                          --     Clock.Shaders.vertex
+                          --     Clock.Shaders.fragment
+                          --     (Clock.Meshes.quad (vec3 0.5 -0.5 0.1) 0.4)
+                          --     (uniforms theta ( w, h ) texture)
+                          -- , WebGL.entityWith
+                          --     [ WebGL.add WebGL.srcAlpha WebGL.oneMinusSrcAlpha ]
+                          --     Clock.Shaders.vertex
+                          --     Clock.Shaders.matte
+                          --     (Clock.Meshes.quad (vec3 theta 0.0 (-10.0 * theta)) 1.0)
+                          --     (Clock.State.cameraUniforms (vec3 1.0 0.0 0.0))
+                          -- ]
                         ]
-                        [ WebGL.entityWith
-                            [ WebGL.add WebGL.srcAlpha WebGL.oneMinusSrcAlpha ]
-                            Clock.Shaders.vertex
-                            Clock.Shaders.fragment
-                            Clock.Meshes.quad
-                            (uniforms theta ( w, h ) texture)
-                        ]
-                    ]
 
                 Nothing ->
                     [ div [ class "error" ] [ text "error loading texture" ] ]
