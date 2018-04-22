5a2677eb707997e753ceca7647456b382095998a
Rogan Murley <rogan@roganmurley.com>
2018-04-22 20:40:58 +0100

    Moved scaling to uniform rather than mesh

--- client/elm/Clock/State.elm
+++ client/elm/Clock/State.elm
@@ -1,7 +1,7 @@
 module Clock.State exposing (..)
 
 import Clock.Types exposing (Model, Uniforms)
-import Math.Matrix4 exposing (Mat4, identity, makeLookAt, makePerspective, makeRotate, mul)
+import Math.Matrix4 exposing (Mat4, identity, makeLookAt, makePerspective, makeRotate, makeScale3, mul)
 import Math.Vector2 exposing (vec2)
 import Math.Vector3 exposing (Vec3, vec3)
 import Raymarch.Types exposing (Height, Width)
@@ -25,6 +25,7 @@ uniforms t ( width, height ) texture pos rot =
     , resolution = vec2 (toFloat width) (toFloat height)
     , texture = texture
     , rotation = rot
+    , scale = makeScale3 0.2 0.2 1
     , worldPos = pos
     , worldRot = makeRotate -t (vec3 0 0 1)
     , perspective = makePerspective 45 ((toFloat width) / (toFloat height)) 0.01 100
@@ -43,21 +44,17 @@ clockFace n origin radius =
         genPoint i =
             ( Math.Vector3.add origin (offset i), rotation i )
 
+        rot : Int -> Float
+        rot i =
+            (toFloat i) * 2.0 * pi / (toFloat n)
+
         offset : Int -> Vec3
         offset i =
-            let
-                rot =
-                    (toFloat i) * 2.0 * pi / (toFloat n)
-            in
-                Math.Vector3.scale radius <|
-                    vec3 (sin rot) (cos rot) 0
+            Math.Vector3.scale radius <|
+                vec3 (sin (rot i)) (cos (rot i)) 0
 
         rotation : Int -> Mat4
         rotation i =
-            let
-                rot =
-                    pi - (toFloat i) * 2.0 * pi / (toFloat n)
-            in
-                makeRotate (rot) (vec3 0 0 1)
+            makeRotate (2 * pi - (rot i)) (vec3 0 0 1)
     in
         List.map genPoint indexes
