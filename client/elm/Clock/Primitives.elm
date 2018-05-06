270a1117c38527417ab47591ae7d4eac8afc762b
Rogan Murley <rogan@roganmurley.com>
2018-05-06 15:36:00 +0100

    Added basic gears to clocks

--- client/elm/Clock/Primitives.elm
+++ client/elm/Clock/Primitives.elm
@@ -49,3 +49,43 @@ circleFragment =
         }
 
     |]
+
+
+gear : Uniforms u -> Entity
+gear =
+    entity
+        gearFragment
+        Clock.Meshes.quad
+
+
+gearFragment : Shader {} (Uniforms u) { vcoord : Vec2 }
+gearFragment =
+    [glsl|
+        precision mediump float;
+
+        uniform vec2 resolution;
+
+        varying vec2 vcoord;
+
+        void main ()
+        {
+            vec2 uv = vcoord;
+
+            vec2 pos = vec2(.5) - uv;
+
+            float r = length(pos) * 2.0;
+            float a = atan(pos.y, pos.x);
+
+            float f = smoothstep(-.5, 1., cos(a * 10.)) * 0.2 + 0.5;
+
+            vec3 color = vec3(1. - smoothstep(f, f + 0.01, r-0.2));
+
+            float alpha = 0.;
+            if (color.x > .99) {
+                alpha = 1.;
+            }
+
+            gl_FragColor = vec4(color, alpha);
+        }
+
+    |]
