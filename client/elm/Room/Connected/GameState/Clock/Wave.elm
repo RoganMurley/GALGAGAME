527d1327ce85ff33369f5ef50342ce9123c1e7ab
Rogan Murley <rogan@potatolondon.com>
2018-10-03 15:36:33 -0400

    Move wave to hurting

--- client/elm/Room/Connected/GameState/Clock/Wave.elm
+++ client/elm/Room/Connected/GameState/Clock/Wave.elm
@@ -34,33 +34,15 @@ view { w, h, radius } resInfo texture =
         progress =
             Ease.outQuad <| resTick / maxTick
 
-        sizeA =
+        waveSize =
             floatInterp progress 0 (3 * radius)
-
-        sizeB =
-            floatInterp progress (0.5 * radius) (3 * radius)
-
-        sizeC =
-            floatInterp progress (0.8 * radius) (3 * radius)
     in
         case anim of
-            Just (Rotate _) ->
+            Just (Slash _ _) ->
                 [ Primitives.circle <|
                     locals texture
                         (vec3 (w / 2) (h / 2) 0)
-                        (makeScale3 sizeA sizeA 1)
-                        (makeRotate 0 (vec3 0 0 1))
-                        (vec3 1 1 1)
-                , Primitives.circle <|
-                    locals texture
-                        (vec3 (w / 2) (h / 2) 0)
-                        (makeScale3 sizeB sizeB 1)
-                        (makeRotate 0 (vec3 0 0 1))
-                        (vec3 1 1 1)
-                , Primitives.circle <|
-                    locals texture
-                        (vec3 (w / 2) (h / 2) 0)
-                        (makeScale3 sizeC sizeC 1)
+                        (makeScale3 waveSize waveSize 1)
                         (makeRotate 0 (vec3 0 0 1))
                         (vec3 1 1 1)
                 ]
