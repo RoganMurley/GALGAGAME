d8235587b18d6e0a585b85897f4dce4fc5df33fc
Rogan Murley <rogan@potatolondon.com>
2018-10-21 10:42:26 +0100

    Align life text

--- client/elm/Room/Connected/GameState/PlayState/Model/View.elm
+++ client/elm/Room/Connected/GameState/PlayState/Model/View.elm
@@ -223,15 +223,17 @@ lifeTextView { radius, model } =
             [ ( "right", 0.49 * radius |> px )
             , ( "top", 0.62 * radius |> px )
             , ( "font-size", 0.18 * radius |> px )
+            , ( "transform", "translate(17%, 10%)" )
             ]
         ]
         [ text <| toString model.life ]
     , div
         [ class "clock-life other"
         , style
-            [ ( "left", 0.5 * radius |> px )
+            [ ( "left", 0.49 * radius |> px )
             , ( "bottom", 0.62 * radius |> px )
             , ( "font-size", 0.18 * radius |> px )
+            , ( "transform", "translate(-28%, -11%)" )
             ]
         ]
         [ text <| toString model.otherLife ]
