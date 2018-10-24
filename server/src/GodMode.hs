21023b51ae2d15521a6a695c0e18a9c37d9233df
Rogan Murley <rogan@potatolondon.com>
2018-10-24 09:01:39 +0100

    Added more god mode commands

--- server/src/GodMode.hs
+++ server/src/GodMode.hs
@@ -1,10 +1,13 @@
 module GodMode where
 
+import Data.List (find)
 import Data.Monoid ((<>))
 import Data.String.Conversions (cs)
 import Data.Text (Text)
 import Safe (readMay)
 
+import Card (Card(..))
+import Cards (allCards)
 import Player (WhichPlayer(..), other)
 import Util (Err, breakAt)
 
@@ -29,5 +32,13 @@ parse which msg =
             Right $ Beta.heal h which
           Nothing ->
             Left ("Cannot parse " <> content <> " to int" :: Err)
+      "draw" ->
+        Right $ Beta.draw which
+      "card" ->
+        case find (\c -> card_name c == content) allCards of
+          Just card ->
+            Right $ Beta.addToHand which card
+          Nothing ->
+            Left ("Unknown card: " <> content :: Err)
       _ ->
         Left ("Unknown commandment: " <> command :: Err)
