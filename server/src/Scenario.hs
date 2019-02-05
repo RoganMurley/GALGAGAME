bc50bc740bf38d82df492e98e70f227ea12c77f7
Rogan Murley <rogan@roganmurley.com>
2019-02-05 09:38:53 +0000

    Initial model setup in scenario

--- server/src/Scenario.hs
+++ server/src/Scenario.hs
@@ -1,8 +1,10 @@
 module Scenario where
 
 import Characters (Character)
+import ModelDiff (ModelDiff)
 
 data Scenario = Scenario
   { scenario_charactersPa :: Maybe (Character, Character, Character)
   , scenario_charactersPb :: Maybe (Character, Character, Character)
+  , scenario_modelDiff    :: ModelDiff
   } deriving (Show)
