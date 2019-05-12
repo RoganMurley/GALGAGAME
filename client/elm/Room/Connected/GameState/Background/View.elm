02b482c2c0e49a758e88ab20c4cd1f91dd6e846a
Rogan Murley <rogan@potatolondon.com>
2019-05-12 11:29:06 +0100

    New ring background for waiting and select

---
+++ client/elm/Room/Connected/GameState/Background/View.elm
@@ -0,0 +1,167 @@
+module Background.View exposing (backgroundView, ornateView, ringView, view)
+
+import Animation.State as Animation
+import Animation.Types exposing (Anim(..))
+import Colour
+import Game.State exposing (bareContextInit)
+import Game.Types exposing (Context)
+import Html exposing (Html, div)
+import Html.Attributes exposing (class, height, width)
+import Main.Types exposing (Flags)
+import Math.Matrix4 exposing (makeLookAt, makeOrtho, makeRotate, makeScale3)
+import Math.Vector3 exposing (vec3)
+import Render.Primitives
+import Render.Shaders
+import Render.Uniforms
+import Texture.State as Texture
+import Texture.Types as Texture
+import WebGL
+import WhichPlayer.Types exposing (WhichPlayer(..))
+
+
+view : Flags -> Texture.Model -> Html msg
+view { dimensions } textures =
+    let
+        ( w, h ) =
+            dimensions
+
+        ctx =
+            bareContextInit dimensions textures
+    in
+    div []
+        [ WebGL.toHtml [ width w, height h, class "webgl-canvas" ] <|
+            backgroundView ctx
+        ]
+
+
+backgroundView : Context -> List WebGL.Entity
+backgroundView ctx =
+    List.concat <|
+        List.map ((|>) ctx)
+            [ ornateView, ringView ]
+
+
+ornateView : Context -> List WebGL.Entity
+ornateView ({ w, h, anim, tick } as ctx) =
+    let
+        color =
+            Colour.darkGray
+
+        scale =
+            0.75
+
+        shake =
+            0.005 * max (Animation.animShake anim PlayerA tick) (Animation.animShake anim PlayerB tick)
+
+        render rotation =
+            [ Render.Primitives.quad Render.Shaders.ornate
+                { rotation = makeRotate (rotation + shake) <| vec3 0 0 1
+                , scale = makeScale3 (scale * w) (scale * h) 1
+                , color = color
+                , pos = vec3 (w * 0.5) (h * 0.5) 0
+                , worldRot = makeRotate 0 <| vec3 0 0 1
+                , perspective = Render.Uniforms.perspective ctx
+                , camera = Render.Uniforms.camera
+                , shift = 0
+                , frequency = 2
+                , amplitude = 0.2
+                , thickness = 0.02
+                }
+            , Render.Primitives.quad Render.Shaders.ornate
+                { rotation = makeRotate (rotation + shake) <| vec3 0 0 1
+                , scale = makeScale3 (scale * w) (-scale * h) 1
+                , color = color
+                , pos = vec3 (w * 0.5) (h * 0.5) 0
+                , worldRot = makeRotate 0 <| vec3 0 0 1
+                , perspective = Render.Uniforms.perspective ctx
+                , camera = Render.Uniforms.camera
+                , shift = 0
+                , frequency = 2
+                , amplitude = 0.2
+                , thickness = 0.02
+                }
+            ]
+    in
+    List.concat <|
+        List.map render <|
+            List.map ((*) pi) <|
+                [ 0, 0.25, 0.5, 0.75, 1.0 ]
+
+
+ringView : Context -> List WebGL.Entity
+ringView { w, h, anim, progress, radius, textures } =
+    let
+        rotationProgress =
+            case anim of
+                Rotate _ ->
+                    progress
+
+                Windup _ ->
+                    1 - progress
+
+                _ ->
+                    0
+
+        rotation =
+            rotationProgress * -2.0 * pi / 12.0
+
+        ringEntity =
+            case Texture.load textures "ring.png" of
+                Nothing ->
+                    []
+
+                Just texture ->
+                    [ Render.Primitives.quad Render.Shaders.fragment
+                        { rotation = makeRotate rotation (vec3 0 0 1)
+                        , scale = makeScale3 (0.77 * radius) (0.77 * radius) 1
+                        , color = Colour.white
+                        , pos = vec3 (w * 0.5) (h * 0.5) 0
+                        , worldRot = makeRotate 0 (vec3 0 0 1)
+                        , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
+                        , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
+                        , texture = texture
+                        }
+                    ]
+
+        lifeclawEntities =
+            case Texture.load textures "lifeclaw.png" of
+                Nothing ->
+                    []
+
+                Just texture ->
+                    [ Render.Primitives.quad Render.Shaders.fragment
+                        { rotation = makeRotate pi (vec3 0 0 1)
+                        , scale = makeScale3 (0.21 * radius) (0.21 * radius) 1
+                        , color = Colour.white
+                        , pos = vec3 (w * 0.5 + 0.65 * radius) (h * 0.5 - 0.65 * radius) 0
+                        , worldRot = makeRotate 0 (vec3 0 0 1)
+                        , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
+                        , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
+                        , texture = texture
+                        }
+                    , Render.Primitives.quad Render.Shaders.fragment
+                        { rotation = makeRotate 0 (vec3 0 0 1)
+                        , scale = makeScale3 (0.21 * radius) (0.21 * radius) 1
+                        , color = Colour.white
+                        , pos = vec3 (w * 0.5 - 0.65 * radius) (h * 0.5 + 0.65 * radius) 0
+                        , worldRot = makeRotate 0 (vec3 0 0 1)
+                        , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
+                        , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
+                        , texture = texture
+                        }
+                    ]
+
+        bgEntity =
+            [ Render.Primitives.quad Render.Shaders.fullCircleFragment
+                { rotation = makeRotate rotation (vec3 0 0 1)
+                , scale = makeScale3 (0.86 * radius) (0.86 * radius) 1
+                , color = vec3 0.38 0.38 0.38
+                , pos = vec3 (w * 0.5) (h * 0.5) 0
+                , worldRot = makeRotate 0 (vec3 0 0 1)
+                , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
+                , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
+                , mag = 1.0
+                }
+            ]
+    in
+    bgEntity ++ lifeclawEntities ++ ringEntity
--- client/elm/Room/Connected/GameState/View.elm
+++ client/elm/Room/Connected/GameState/View.elm
@@ -1,19 +1,18 @@
 module GameState.View exposing (view)
 
+import Background.View as Background
 import CharacterSelect.View as CharacterSelect
 import Connected.Messages as Connected
-import Game.State exposing (bareContextInit)
 import GameState.Messages exposing (Msg(..))
 import GameState.Types exposing (GameState(..), WaitType(..))
 import Html exposing (Html, button, div, input, text)
-import Html.Attributes exposing (class, height, id, readonly, type_, value, width)
+import Html.Attributes exposing (class, id, readonly, type_, value)
 import Html.Events exposing (onClick)
 import Main.Messages as Main
 import Main.Types exposing (Flags)
 import PlayState.View as PlayState
 import Room.Messages as Room
 import Texture.Types as Texture
-import WebGL
 
 
 view : GameState -> String -> Flags -> Texture.Model -> Html Main.Msg
@@ -75,7 +74,7 @@ waitingView waitType ({ httpPort, hostname } as flags) textures roomID =
             case waitType of
                 WaitCustom ->
                     div []
-                        [ ringView flags textures
+                        [ Background.view flags textures
                         , div [ class "input-group" ]
                             [ input
                                 [ value challengeLink
@@ -92,34 +91,10 @@ waitingView waitType ({ httpPort, hostname } as flags) textures roomID =
                         ]
 
                 WaitQuickplay ->
-                    ringView flags textures
+                    Background.view flags textures
     in
     div [ class "waiting" ]
         [ div [ class "waiting-prompt" ]
             [ text waitingPrompt ]
         , waitingInfo
-        ]
-
-
-ringView : Flags -> Texture.Model -> Html msg
-ringView { dimensions } textures =
-    let
-        ( w, h ) =
-            dimensions
-
-        ctx =
-            bareContextInit ( w, h ) textures
-    in
-    div []
-        [ WebGL.toHtml
-            [ width <| w
-            , height <| h
-            , class "webgl-canvas"
-            ]
-          <|
-            List.concat <|
-                List.map ((|>) ctx)
-                    [ CharacterSelect.backgroundRingView
-                    , CharacterSelect.circlesView
-                    ]
         ]
