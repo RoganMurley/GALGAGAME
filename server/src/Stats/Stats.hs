8abb6f1c782d7597c84d788ccac863b24c14048d
Rogan Murley <rogan@roganmurley.com>
2019-06-03 11:52:14 +0100

    Experience tweaks

--- server/src/Stats/Stats.hs
+++ server/src/Stats/Stats.hs
@@ -5,5 +5,17 @@ import Data.Int (Int64)
 type Experience = Int64
 type Level = Int
 
+levellingConstant :: Float
+levellingConstant = 0.2
+
 levelFromExperience :: Experience -> Level
-levelFromExperience xp = floor $ 0.2 * sqrt xp
+levelFromExperience xp = floor $ levellingConstant * sqrt (fromIntegral xp)
+
+levelToExperience :: Level -> Experience
+levelToExperience level = floor $ (fromIntegral level / levellingConstant) ** 2
+
+nextLevelAt :: Experience -> Experience
+nextLevelAt xp = levelToExperience nextLevel
+  where
+    currentLevel = levelFromExperience xp :: Level
+    nextLevel = currentLevel + 1 :: Level
