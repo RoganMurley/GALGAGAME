46dcc80dea5b4550a6115f9b6e416d5dd7672c94
Rogan Murley <rogan@roganmurley.com>
2019-06-24 23:54:51 +0100

    Settings menu tweaks

--- client/elm/Settings/View.elm
+++ client/elm/Settings/View.elm
@@ -1,7 +1,7 @@
 module Settings.View exposing (view)
 
-import Html exposing (Html, button, div, h1, img, input, label, text)
-import Html.Attributes as H exposing (attribute, class, src, style, type_, value)
+import Html exposing (Html, div, h1, img, input, label, text)
+import Html.Attributes as H exposing (class, src, style, type_, value)
 import Html.Events exposing (onClick, onInput)
 import Main.Messages as Main
 import Settings.Messages as Settings
@@ -9,7 +9,7 @@ import Settings.Types exposing (ModalState(..), Model, VolumeType(..))
 
 
 view : Model -> List (Html Main.Msg) -> Html Main.Msg
-view { modalState, masterVolume, musicVolume, sfxVolume } nestedViews =
+view { modalState, masterVolume } nestedViews =
     let
         settingsStyle =
             case modalState of
@@ -35,57 +35,58 @@ view { modalState, masterVolume, musicVolume, sfxVolume } nestedViews =
             [ div
                 [ class "settings-body"
                 ]
-                [ div [ class "settings-inner" ]
-                    [ h1 [] [ text "Settings" ]
-                    , label [ class "settings-volume" ]
-                        [ text "Master Volume"
-                        , input
-                            [ class "settings-slider"
-                            , type_ "range"
-                            , H.min "0"
-                            , H.max "100"
-                            , value <| String.fromInt masterVolume
-                            , onInput
-                                (\v -> Main.SetVolume Master <| Maybe.withDefault 0 (String.toInt v))
-                            ]
-                            []
+                [ h1 [] [ text "Settings" ]
+                , label [ class "settings-volume" ]
+                    [ text "Volume"
+                    , input
+                        [ class "settings-slider"
+                        , type_ "range"
+                        , H.min "0"
+                        , H.max "100"
+                        , value <| String.fromInt masterVolume
+                        , onInput
+                            (\v -> Main.SetVolume Master <| Maybe.withDefault 0 (String.toInt v))
                         ]
-                    , label [ class "settings-volume" ]
-                        [ text "Music Volume"
-                        , input
-                            [ class "settings-slider"
-                            , type_ "range"
-                            , H.min "0"
-                            , H.max "100"
-                            , value <| String.fromInt musicVolume
-                            , onInput
-                                (\v -> Main.SetVolume Music <| Maybe.withDefault 0 (String.toInt v))
-                            ]
-                            []
-                        ]
-                    , label [ class "settings-volume" ]
-                        [ text "SFX Volume"
-                        , input
-                            [ class "settings-slider"
-                            , type_ "range"
-                            , H.min "0"
-                            , H.max "100"
-                            , value <| String.fromInt sfxVolume
-                            , onInput
-                                (\v -> Main.SetVolume Sfx <| Maybe.withDefault 0 (String.toInt v))
-                            ]
-                            []
-                        ]
-                    , div [] <|
-                        -- onclick hack here as fullscreen requires user interaction.
-                        List.concat
-                            [ [ button
-                                    [ class "settings-button", attribute "onclick" "window.requestFullscreen()" ]
-                                    [ text "Fullscreen" ]
-                              ]
-                            , nestedViews
-                            ]
+                        []
                     ]
+
+                -- , label [ class "settings-volume" ]
+                --     [ text "Music Volume"
+                --     , input
+                --         [ class "settings-slider"
+                --         , type_ "range"
+                --         , H.min "0"
+                --         , H.max "100"
+                --         , value <| String.fromInt musicVolume
+                --         , onInput
+                --             (\v -> Main.SetVolume Music <| Maybe.withDefault 0 (String.toInt v))
+                --         ]
+                --         []
+                --     ]
+                -- , label [ class "settings-volume" ]
+                --     [ text "SFX Volume"
+                --     , input
+                --         [ class "settings-slider"
+                --         , type_ "range"
+                --         , H.min "0"
+                --         , H.max "100"
+                --         , value <| String.fromInt sfxVolume
+                --         , onInput
+                --             (\v -> Main.SetVolume Sfx <| Maybe.withDefault 0 (String.toInt v))
+                --         ]
+                --         []
+                -- ]
+                , div [] <|
+                    -- onclick hack here as fullscreen requires user interaction.
+                    List.concat
+                        -- onclick was broken in elm 0.19
+                        [ -- [ [ button
+                          --         [ class "settings-button", attribute "onclick" "window.requestFullscreen()" ]
+                          --         [ text "Fullscreen" ]
+                          --   ]
+                          -- , nestedViews
+                          nestedViews
+                        ]
                 ]
             ]
         ]
