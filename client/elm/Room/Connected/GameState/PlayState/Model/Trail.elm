178f048ac80980c9c48bd2a287a6a11d1b78e6a0
Rogan Murley <rogan@potatolondon.com>
2019-08-28 22:59:59 +0100

    Added drawing card trails

--- client/elm/Room/Connected/GameState/PlayState/Model/Trail.elm
+++ client/elm/Room/Connected/GameState/PlayState/Model/Trail.elm
@@ -2,7 +2,7 @@ module Trail exposing (view)
 
 import Animation.State as Animation
 import Animation.Types exposing (Anim(..))
-import Colour
+import Colour exposing (Colour)
 import Ease
 import Game.Types exposing (Context, Hover(..))
 import Hand.Entities exposing (handCardPosition, playPosition)
@@ -23,6 +23,21 @@ toShaderSpace w h screenSpace =
             Math.Vector2.toRecord screenSpace
     in
     vec2 (x / w) (1 - y / h)
+
+
+trailQuad : Colour -> Vec2 -> Vec2 -> Context -> WebGL.Entity
+trailQuad colour start end { w, h } =
+    Render.Primitives.quad Render.Shaders.trail
+        { rotation = makeRotate pi (vec3 0 0 1)
+        , scale = makeScale3 (0.5 * w) (0.5 * h) 1
+        , color = colour
+        , pos = vec3 (w * 0.5) (h * 0.5) 0
+        , worldRot = makeRotate 0 (vec3 0 0 1)
+        , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
+        , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
+        , start = start
+        , end = end
+        }
 
 
 view : Context -> List WebGL.Entity
@@ -55,19 +70,12 @@ view ({ anim, model, progress, w, h, tick } as ctx) =
                 end : Vec2
                 end =
                     interp2D progress start final
+
+                colour : Colour
+                colour =
+                    Colour.card PlayerA
             in
-            [ Render.Primitives.quad Render.Shaders.trail
-                { rotation = makeRotate pi (vec3 0 0 1)
-                , scale = makeScale3 (0.5 * w) (0.5 * h) 1
-                , color = Colour.card PlayerA
-                , pos = vec3 (w * 0.5) (h * 0.5) 0
-                , worldRot = makeRotate 0 (vec3 0 0 1)
-                , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
-                , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
-                , start = start
-                , end = end
-                }
-            ]
+            [ trailQuad colour start end ctx ]
 
         Play PlayerB _ i ->
             let
@@ -91,19 +99,70 @@ view ({ anim, model, progress, w, h, tick } as ctx) =
                 end : Vec2
                 end =
                     interp2D progress start final
+
+                colour : Colour
+                colour =
+                    Colour.card PlayerB
             in
-            [ Render.Primitives.quad Render.Shaders.trail
-                { rotation = makeRotate pi (vec3 0 0 1)
-                , scale = makeScale3 (0.5 * w) (0.5 * h) 1
-                , color = Colour.card PlayerB
-                , pos = vec3 (w * 0.5) (h * 0.5) 0
-                , worldRot = makeRotate 0 (vec3 0 0 1)
-                , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
-                , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
-                , start = start
-                , end = end
-                }
-            ]
+            [ trailQuad colour start end ctx ]
+
+        Draw PlayerA ->
+            let
+                n : Int
+                n =
+                    List.length model.hand
+
+                initial : Vec2
+                initial =
+                    toShaderSpace w h <| vec2 w h
+
+                final : Vec2
+                final =
+                    toShaderSpace w h <|
+                        handCardPosition ctx PlayerA n (n + 1) NoHover
+
+                start : Vec2
+                start =
+                    interp2D trailProgress initial final
+
+                end : Vec2
+                end =
+                    interp2D progress start final
+
+                colour : Colour
+                colour =
+                    Colour.card PlayerA
+            in
+            [ trailQuad colour start end ctx ]
+
+        Draw PlayerB ->
+            let
+                n : Int
+                n =
+                    model.otherHand
+
+                initial : Vec2
+                initial =
+                    toShaderSpace w h <| vec2 w 0
+
+                final : Vec2
+                final =
+                    toShaderSpace w h <|
+                        handCardPosition ctx PlayerB n (n + 1) NoHover
+
+                start : Vec2
+                start =
+                    interp2D trailProgress initial final
+
+                end : Vec2
+                end =
+                    interp2D progress start final
+
+                colour : Colour
+                colour =
+                    Colour.card PlayerB
+            in
+            [ trailQuad colour start end ctx ]
 
         _ ->
             []
