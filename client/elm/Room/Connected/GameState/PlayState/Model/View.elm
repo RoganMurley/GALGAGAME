ab06419d82619626b36ba912422764d7385396c1
Rogan Murley <rogan@roganmurley.com>
2020-06-29 17:56:11 +0100

    WebGL turn indicator

--- client/elm/Room/Connected/GameState/PlayState/Model/View.elm
+++ client/elm/Room/Connected/GameState/PlayState/Model/View.elm
@@ -33,8 +33,6 @@ import Render.Uniforms exposing (uniColourMag)
 import Room.Messages as Room
 import Stack.Types exposing (StackCard)
 import Stack.View as Stack
-import Texture.State as Texture
-import Texture.Types as Texture
 import Trail
 import Util exposing (interpFloat, px)
 import WebGL
@@ -46,9 +44,6 @@ view { w, h, pixelRatio } { res, hover, focus, entities, passed, feedback, vfx }
     let
         ctx =
             contextInit ( w, h ) res assets Nothing
-
-        ( turnHtml, turnWebGl ) =
-            turnView ctx focus passed
     in
     div [ class "clock" ]
         [ WebGL.toHtml [ width <| floor <| toFloat w * pixelRatio, height <| floor <| toFloat h * pixelRatio, class "webgl-canvas" ]
@@ -68,16 +63,15 @@ view { w, h, pixelRatio } { res, hover, focus, entities, passed, feedback, vfx }
                     , Trail.view
                     , Hand.view entities.hand
                     , Hand.otherView entities.otherHand
-                    , \_ -> turnWebGl
                     , Hand.millView
 
                     -- , Background.cursorView
                     , damageWebGl hover
+                    , turnView focus passed
                     , feedbackView feedback
                     ]
             )
         , div [ class "text-focus" ] [ focusTextView ctx focus ]
-        , turnHtml
         , goButtonView ctx passed
         ]
 
@@ -387,104 +381,119 @@ goButtonView { model, radius, resolving } passed =
         [ text "GO" ]
 
 
-turnView : Context -> Maybe StackCard -> Bool -> ( Html Main.Msg, List WebGL.Entity )
-turnView { anim, model, tick, w, h, radius, textures } focus passed =
+turnView : Maybe StackCard -> Bool -> Context -> List WebGL.Entity
+turnView focus passed ctx =
     let
+        { anim, model, tick, w, h, radius } =
+            ctx
+
         size =
             radius * 3
     in
     case ( anim, focus, passed ) of
         ( Mill _ _, _, _ ) ->
-            ( div [] [], [] )
+            []
 
         ( NullAnim, Nothing, False ) ->
             case model.turn of
                 PlayerA ->
-                    ( div [] []
-                    , Texture.with textures "yourTurn.png" <|
-                        \texture ->
-                            [ Render.Primitives.quad Render.Shaders.fragment
-                                { rotation = makeRotate pi (vec3 0 0 1)
-                                , scale = makeScale3 (0.15 * size + sin (tick * 0.005)) (0.15 * size + sin (tick * 0.007)) 1
-                                , color = vec3 (20 / 255) (20 / 255) (20 / 255)
-                                , pos = vec3 (w * 0.5 - 0.003 * size) (h * 0.5) 0
-                                , worldRot = makeRotate 0 (vec3 0 0 1)
-                                , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
-                                , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
-                                , texture = texture
-                                }
-                            , Render.Primitives.quad Render.Shaders.fragment
-                                { rotation = makeRotate pi (vec3 0 0 1)
-                                , scale = makeScale3 (0.15 * size + sin (tick * 0.005)) (0.15 * size + sin (tick * 0.007)) 1
-                                , color = vec3 (244 / 255) (241 / 255) (94 / 255)
-                                , pos = vec3 (w * 0.5) (h * 0.5) 0
-                                , worldRot = makeRotate 0 (vec3 0 0 1)
-                                , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
-                                , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
-                                , texture = texture
-                                }
-                            ]
-                    )
+                    List.concat
+                        [ Font.view
+                            "Rock Salt"
+                            "Your Turn"
+                            { x = w * 0.5 - 0.003 * size
+                            , y = h * 0.5
+                            , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+                            , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+                            , color = vec3 (20 / 255) (20 / 255) (20 / 255)
+                            }
+                            ctx
+                        , Font.view
+                            "Rock Salt"
+                            "Your Turn"
+                            { x = w * 0.5
+                            , y = h * 0.5
+                            , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+                            , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+                            , color = vec3 (244 / 255) (241 / 255) (94 / 255)
+                            }
+                            ctx
+                        ]
 
                 PlayerB ->
-                    ( div [] []
-                    , Texture.with textures "theirTurn.png" <|
-                        \texture ->
-                            [ Render.Primitives.quad Render.Shaders.fragment
-                                { rotation = makeRotate pi (vec3 0 0 1)
-                                , scale = makeScale3 (0.15 * size + sin (tick * 0.005)) (0.15 * size + sin (tick * 0.007)) 1
-                                , color = vec3 (20 / 255) (20 / 255) (20 / 255)
-                                , pos = vec3 (w * 0.5 - 0.003 * size) (h * 0.5) 0
-                                , worldRot = makeRotate 0 (vec3 0 0 1)
-                                , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
-                                , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
-                                , texture = texture
-                                }
-                            , Render.Primitives.quad Render.Shaders.fragment
-                                { rotation = makeRotate pi (vec3 0 0 1)
-                                , scale = makeScale3 (0.15 * size + sin (tick * 0.005)) (0.15 * size + sin (tick * 0.007)) 1
-                                , color = vec3 (244 / 255) (241 / 255) (94 / 255)
-                                , pos = vec3 (w * 0.5) (h * 0.5) 0
-                                , worldRot = makeRotate 0 (vec3 0 0 1)
-                                , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
-                                , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
-                                , texture = texture
-                                }
-                            ]
-                    )
+                    List.concat
+                        [ Font.view
+                            "Rock Salt"
+                            "Their Turn"
+                            { x = w * 0.5 - 0.003 * size
+                            , y = h * 0.5
+                            , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+                            , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+                            , color = vec3 (20 / 255) (20 / 255) (20 / 255)
+                            }
+                            ctx
+                        , Font.view
+                            "Rock Salt"
+                            "Their Turn"
+                            { x = w * 0.5
+                            , y = h * 0.5
+                            , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+                            , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+                            , color = vec3 (244 / 255) (241 / 255) (94 / 255)
+                            }
+                            ctx
+                        ]
 
         ( Pass _, _, _ ) ->
-            ( div [] []
-            , Texture.with textures "pass.png" <|
-                \texture ->
-                    [ Render.Primitives.quad Render.Shaders.fragment
-                        { rotation = makeRotate pi (vec3 0 0 1)
-                        , scale = makeScale3 (0.15 * size + sin (tick * 0.005)) (0.15 * size + sin (tick * 0.007)) 1
-                        , color = vec3 (20 / 255) (20 / 255) (20 / 255)
-                        , pos = vec3 (w * 0.5 - 0.003 * size) (h * 0.5) 0
-                        , worldRot = makeRotate 0 (vec3 0 0 1)
-                        , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
-                        , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
-                        , texture = texture
-                        }
-                    , Render.Primitives.quad Render.Shaders.fragment
-                        { rotation = makeRotate pi (vec3 0 0 1)
-                        , scale = makeScale3 (0.15 * size + sin (tick * 0.005)) (0.15 * size + sin (tick * 0.007)) 1
-                        , color = vec3 (244 / 255) (241 / 255) (94 / 255)
-                        , pos = vec3 (w * 0.5) (h * 0.5) 0
-                        , worldRot = makeRotate 0 (vec3 0 0 1)
-                        , perspective = makeOrtho 0 (w / 2) (h / 2) 0 0.01 1000
-                        , camera = makeLookAt (vec3 0 0 1) (vec3 0 0 0) (vec3 0 1 0)
-                        , texture = texture
-                        }
-                    ]
-            )
+            List.concat
+                [ Font.view
+                    "Rock Salt"
+                    "Pass"
+                    { x = w * 0.5 - 0.003 * size
+                    , y = h * 0.5
+                    , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+                    , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+                    , color = vec3 (20 / 255) (20 / 255) (20 / 255)
+                    }
+                    ctx
+                , Font.view
+                    "Rock Salt"
+                    "Pass"
+                    { x = w * 0.5
+                    , y = h * 0.5
+                    , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+                    , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+                    , color = vec3 (244 / 255) (241 / 255) (94 / 255)
+                    }
+                    ctx
+                ]
 
         ( HandFullPass, _, _ ) ->
-            ( div [ class "pass-status" ] [ text "HAND FULL" ], [] )
+            List.concat
+                [ Font.view
+                    "Rock Salt"
+                    "Hand Full"
+                    { x = w * 0.5 - 0.003 * size
+                    , y = h * 0.5
+                    , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+                    , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+                    , color = vec3 (20 / 255) (20 / 255) (20 / 255)
+                    }
+                    ctx
+                , Font.view
+                    "Rock Salt"
+                    "Hand Full"
+                    { x = w * 0.5
+                    , y = h * 0.5
+                    , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+                    , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+                    , color = vec3 (244 / 255) (241 / 255) (94 / 255)
+                    }
+                    ctx
+                ]
 
         _ ->
-            ( div [] [], [] )
+            []
 
 
 passView : Context -> List WebGL.Entity
