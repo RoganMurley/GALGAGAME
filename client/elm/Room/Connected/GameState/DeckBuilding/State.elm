630d20e647d73edad0ad8f2231069d21fc6d4581
Rogan Murley <rogan@potatolondon.com>
2020-08-21 18:57:04 +0100

    Nice button bounce

--- client/elm/Room/Connected/GameState/DeckBuilding/State.elm
+++ client/elm/Room/Connected/GameState/DeckBuilding/State.elm
@@ -1,11 +1,13 @@
 module DeckBuilding.State exposing (getRuneFromCursor, init, mouseClick, nextCursor, tick, update)
 
 import Carousel
+import Collision exposing (hitTest)
 import DeckBuilding.Encoders exposing (encodeCharacter)
 import DeckBuilding.Messages exposing (Msg(..))
-import DeckBuilding.Types exposing (Character, Model)
+import DeckBuilding.Types exposing (Button, Character, Model)
 import Game.Types exposing (Context)
 import Main.Messages as Main
+import Math.Vector2 exposing (vec2)
 import Mouse exposing (Position)
 import Ports exposing (log)
 import RuneSelect.State as RuneSelect
@@ -22,6 +24,7 @@ init character remaining runes =
     , ready = False
     , bounceTick = 0
     , vfx = Vfx.init
+    , buttons = { ready = Nothing }
     }
 
 
@@ -113,6 +116,43 @@ tick ctx dt model =
         | runeSelect = newRuneSelect
         , bounceTick = model.bounceTick + dt
         , vfx = Vfx.tick dt model.vfx ctx
+        , buttons = { ready = Just <| readyButton model.buttons.ready dt ctx }
+    }
+
+
+readyButton : Maybe Button -> Float -> Context -> Button
+readyButton mButton dt { mouse, w, h } =
+    let
+        x =
+            w * 0.5
+
+        y =
+            h * 0.8
+
+        previousHover =
+            case mButton of
+                Just button ->
+                    button.hover
+
+                Nothing ->
+                    0
+
+        hover =
+            case mouse of
+                Just m ->
+                    if hitTest m 100 { position = vec2 x y } then
+                        min (previousHover + dt) 300
+
+                    else
+                        0
+
+                Nothing ->
+                    0
+    in
+    { x = x
+    , y = y
+    , hover = hover
+    , size = 0.6 * max w h
     }
 
 
@@ -157,6 +197,22 @@ nextCursor cursor =
 
 mouseClick : Position -> Model -> ( Model, Cmd Main.Msg )
 mouseClick _ model =
-    update
-        (Select model.characters.selected)
-        model
+    case model.runeSelect of
+        Nothing ->
+            let
+                hit =
+                    case model.buttons.ready of
+                        Just { hover } ->
+                            hover > 0
+
+                        Nothing ->
+                            False
+            in
+            if hit then
+                update (Select model.characters.selected) model
+
+            else
+                ( model, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
--- client/elm/Room/Connected/GameState/DeckBuilding/View.elm
+++ client/elm/Room/Connected/GameState/DeckBuilding/View.elm
@@ -2,13 +2,18 @@ module DeckBuilding.View exposing (webglView)
 
 import Assets.Types as Assets
 import Background.View exposing (radialView)
+import Carousel exposing (Carousel)
 import DeckBuilding.Messages exposing (Msg(..))
-import DeckBuilding.Types exposing (Model)
+import DeckBuilding.Types exposing (Button, Character, Model)
+import Ease
 import Font.Types as Font
 import Font.View as Font
 import Game.State exposing (bareContextInit)
 import Game.Types exposing (Context)
+import Math.Matrix4 exposing (makeRotate, makeScale3)
 import Math.Vector3 exposing (vec3)
+import Render.Primitives
+import Render.Shaders
 import Render.Types as Render
 import RuneSelect.Types as RuneSelect exposing (RuneCursor(..))
 import RuneSelect.View as RuneSelect
@@ -23,7 +28,7 @@ webglView { w, h } model assets =
         ctx =
             bareContextInit ( w, h ) assets Nothing
     in
-    List.concat <|
+    (List.concat <|
         List.map ((|>) ctx) <|
             case model.runeSelect of
                 Just runeSelect ->
@@ -31,35 +36,116 @@ webglView { w, h } model assets =
 
                 Nothing ->
                     [ radialView model.vfx
-                    , titleView model.vfx.rotation
+
+                    -- , titleView model.vfx.rotation
+                    , charactersView model.characters model.vfx.rotation
                     ]
+    )
+        ++ (case model.buttons.ready of
+                Just button ->
+                    buttonView button model.vfx.rotation ctx
+
+                Nothing ->
+                    []
+           )
 
 
-titleView : Float -> Context -> List WebGL.Entity
-titleView tick ({ w, h } as ctx) =
+charactersView : Carousel Character -> Float -> Context -> List WebGL.Entity
+charactersView characters tick ctx =
     let
+        character =
+            characters.selected
+
+        { w, h } =
+            ctx
+
         size =
             1.4 * max w h
     in
     List.concat
         [ Font.view
             "Futura"
-            "GALGAGAME"
+            character.name
             { x = w * 0.5 - 0.003 * size
-            , y = h * 0.5
-            , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
-            , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
-            , color = vec3 (20 / 255) (20 / 255) (20 / 255)
-            }
-            ctx
-        , Font.view
-            "Futura"
-            "GALGAGAME"
-            { x = w * 0.5
-            , y = h * 0.5
+            , y = h * 0.2
             , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
             , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
             , color = vec3 (244 / 255) (241 / 255) (94 / 255)
             }
             ctx
         ]
+
+
+buttonView : Button -> Float -> Context -> List WebGL.Entity
+buttonView button _ ctx =
+    let
+        { ortho, camera2d } =
+            ctx
+
+        ( textColor, bgColor ) =
+            if button.hover > 0 then
+                ( vec3 (0 / 255) (0 / 255) (0 / 255), vec3 (255 / 255) (255 / 255) (0 / 255) )
+
+            else
+                ( vec3 (0 / 255) (0 / 255) (0 / 255), vec3 (244 / 255) (241 / 255) (94 / 255) )
+
+        hoverPop =
+            10 * Ease.outQuint (button.hover / 300)
+    in
+    List.concat
+        [ [ Render.Primitives.quad Render.Shaders.matte
+                { rotation = makeRotate pi (vec3 0 0 1)
+                , scale =
+                    makeScale3
+                        (0.12 * button.size + hoverPop)
+                        (0.05 * button.size + hoverPop)
+                        1
+                , color = bgColor
+                , alpha = 1
+                , pos = vec3 button.x button.y 0
+                , perspective = ortho
+                , camera = camera2d
+                }
+          ]
+        , Font.view
+            "Futura"
+            "READY"
+            { x = button.x
+            , y = button.y
+            , scaleX = 0.0001 * button.size + 0.001 * hoverPop
+            , scaleY = 0.0001 * button.size + 0.001 * hoverPop
+            , color = textColor
+            }
+            ctx
+        ]
+
+
+
+-- titleView : Float -> Context -> List WebGL.Entity
+-- titleView tick ({ w, h } as ctx) =
+--     let
+--         size =
+--             1.4 * max w h
+--     in
+--     List.concat
+--         [ Font.view
+--             "Futura"
+--             "GALGAGAME"
+--             { x = w * 0.5 - 0.003 * size
+--             , y = h * 0.5
+--             , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+--             , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+--             , color = vec3 (20 / 255) (20 / 255) (20 / 255)
+--             }
+--             ctx
+--         , Font.view
+--             "Futura"
+--             "GALGAGAME"
+--             { x = w * 0.5
+--             , y = h * 0.5
+--             , scaleX = 0.0001 * size + 0.003 * sin (tick * 0.005)
+--             , scaleY = 0.0001 * size + 0.003 * sin (tick * 0.007)
+--             , color = vec3 (244 / 255) (241 / 255) (94 / 255)
+--             }
+--             ctx
+--         ]
