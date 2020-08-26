0cda5ddb3996be7ef240be743b418478ea45b04a
Rogan Murley <rogan@roganmurley.com>
2020-08-26 11:01:25 +0100

    Font manifest

--- client/elm/Assets/Manifest/State.elm
+++ client/elm/Assets/Manifest/State.elm
@@ -3,6 +3,7 @@ module Manifest.State exposing (fetch, init, update)
 import Assets.Fetch as Assets
 import Assets.Messages as Assets
 import Assets.Types as Assets
+import Font.State as Font
 import Http
 import Main.Messages as Main
 import Manifest.Decoders as Manifest
@@ -22,13 +23,19 @@ update msg m =
     case msg of
         ManifestLoaded ( _, manifest ) ->
             let
+                fetchFont : List (Cmd Main.Msg)
+                fetchFont =
+                    List.map
+                        (Cmd.map (Main.AssetsMsg << Assets.FontMsg))
+                        (Font.fetch manifest)
+
                 fetchTextures : List (Cmd Main.Msg)
                 fetchTextures =
                     List.map
                         (Cmd.map (Main.AssetsMsg << Assets.TextureMsg))
                         (Texture.fetch manifest)
             in
-            ( Just manifest, Cmd.batch fetchTextures )
+            ( Just manifest, Cmd.batch <| fetchFont ++ fetchTextures )
 
         ManifestError errorStr ->
             ( m, log <| "Error loading asset manifest: " ++ errorStr )
