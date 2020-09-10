4b01b119fcbd74360e830b0101a34eea701af92f
Rogan Murley <rogan@roganmurley.com>
2020-09-10 10:37:42 +0100

    Update life orb colors

--- client/elm/Colour.elm
+++ client/elm/Colour.elm
@@ -43,20 +43,20 @@ card : WhichPlayer -> Colour
 card which =
     case which of
         PlayerA ->
-            vec3 0.95 0.27 0.29
+            vec3 (255 / 255) (59 / 255) (59 / 255)
 
         PlayerB ->
-            vec3 0.14 0.52 0.75
+            vec3 (41 / 255) (171 / 255) (226 / 255)
 
 
 background : WhichPlayer -> Colour
 background which =
     case which of
         PlayerA ->
-            vec3 0.75 0.07 0.09
+            vec3 (237 / 255) (28 / 255) (36 / 255)
 
         PlayerB ->
-            vec3 0.0 0.32 0.55
+            vec3 (0 / 255) (113 / 255) (188 / 255)
 
 
 focusBackground : WhichPlayer -> Colour
