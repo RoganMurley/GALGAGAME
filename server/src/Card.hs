852ed364c71d9a10b7f7683892b1779590ad54a2
Rogan Murley <rogan@potatolondon.com>
2020-10-27 01:47:36 +0000

    Add aspects to card data type

--- server/src/Card.hs
+++ server/src/Card.hs
@@ -10,8 +10,8 @@ import {-# SOURCE #-} qualified DSL.Beta.DSL as Beta
 
 
 instance Eq Card where
-  (Card n1 d1 i1 s1 _) == (Card n2 d2 i2 s2 _) =
-    n1 == n2 && d1 == d2 && i1 == i2 && s1 == s2
+  (Card n1 d1 i1 a1 s1 _) == (Card n2 d2 i2 a2 s2 _) =
+    n1 == n2 && d1 == d2 && i1 == i2 && a1 == a2 && s1 == s2
 
 
 instance Show Card where
@@ -19,20 +19,21 @@ instance Show Card where
 
 
 instance ToJSON Card where
-  toJSON (Card name desc imageURL _ _) =
+  toJSON (Card{ card_name, card_desc, card_img }) =
     object
-      [ "name"     .= toUpper name
-      , "desc"     .= desc
-      , "imageURL" .= imageURL
+      [ "name"     .= toUpper card_name
+      , "desc"     .= card_desc
+      , "imageURL" .= card_img
       ]
 
 
 data Card = Card
-  { card_name :: Text
-  , card_desc :: Text
-  , card_img  :: Text
-  , card_suit :: Suit
-  , card_eff  :: WhichPlayer -> Beta.Program ()
+  { card_name   :: Text
+  , card_desc   :: Text
+  , card_img    :: Text
+  , card_aspect :: Aspect
+  , card_suit   :: Suit
+  , card_eff    :: WhichPlayer -> Beta.Program ()
   }
 
 
@@ -42,5 +43,9 @@ description Card{ card_name, card_desc } =
   "(" <> card_name <> ": " <> card_desc <> ")"
 
 
-data Suit = Sword | Wand | Cup | Coin | Other
+data Suit = Sword | Wand | Cup | Coin | OtherSuit
+  deriving Eq
+
+
+data Aspect = Blaze | Heaven | Duality | Shroom | Blood | Mirage | Mirror | Alchemy | Crown | Morph | Strange | OtherAspect
   deriving Eq
