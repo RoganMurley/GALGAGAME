1bb756d9aeced135cb7715a8323507cf58b6ec47
Rogan Murley <rogan@roganmurley.com>
2022-02-20 18:10:36 +0000

    Better tutorial

--- server/src/Start.hs
+++ server/src/Start.hs
@@ -6,8 +6,6 @@ import qualified DSL.Alpha as Alpha
 import qualified DSL.Beta as Beta
 import Model (Turn, maxHandLength)
 import Player (WhichPlayer (..))
-import qualified Stack
-import StackCard (StackCard (..))
 
 initHandLength :: WhichPlayer -> Turn -> Int
 initHandLength which first
@@ -22,29 +20,19 @@ startProgram turn = do
 tutorialProgram :: Beta.Program ()
 tutorialProgram = do
   Beta.raw $ do
-    let deckA = take 25 $ cycle [Cards.blazeSword, Cards.blazeSword, Cards.blazeWand]
+    let makeDeck = take 25 . cycle
+    let deckA = makeDeck [Cards.blazeSword, Cards.blazeWand, Cards.blazeGrail, Cards.blazeCoin]
     Alpha.setDeck PlayerA deckA
     Alpha.setMaxLife PlayerA 20
-    Alpha.setLife PlayerA 20
+    Alpha.setLife PlayerA 50
 
-    let deckB = take 25 $ cycle [Cards.alchemyGrail]
+    let deckB = makeDeck [Cards.alchemySword, Cards.alchemyWand, Cards.alchemyGrail, Cards.alchemyCoin]
     Alpha.setDeck PlayerB deckB
     Alpha.setMaxLife PlayerB 20
     Alpha.setLife PlayerB 20
   Beta.null
   replicateM_ 5 (Beta.draw PlayerA PlayerA 0.25)
   replicateM_ 5 (Beta.draw PlayerB PlayerB 0.25)
-
--- tutorialProgram :: Beta.Program ()
--- tutorialProgram = do
---   Beta.raw $ do
---     let deckA = take 25 $ cycle [Cards.heavenSword, Cards.heavenSword, Cards.heavenWand, Cards.heavenGrail, Cards.heavenCoin]
---     Alpha.setDeck PlayerA deckA
---     let deckB = take 25 $ cycle [Cards.blazeSword, Cards.blazeSword, Cards.blazeSword, Cards.blazeGrail, Cards.blazeWand]
---     Alpha.setDeck PlayerB deckB
---   Beta.null
---   replicateM_ 5 (Beta.draw PlayerA PlayerA 0.25)
---   replicateM_ 5 (Beta.draw PlayerB PlayerB 0.25)
 
 puzzle :: Beta.Program ()
 puzzle = do
@@ -56,7 +44,6 @@ puzzle = do
     Alpha.setMaxLife PlayerA 15
     Alpha.setMaxLife PlayerB 15
     Alpha.setTurn PlayerB
-  -- Alpha.modStack (\s -> Stack.set s 1 (Just (StackCard PlayerB Cards.blazeSword)))
   Beta.null
   replicateM_ 4 (Beta.draw PlayerA PlayerA 0.25)
   replicateM_ 5 (Beta.draw PlayerB PlayerB 0.25)
