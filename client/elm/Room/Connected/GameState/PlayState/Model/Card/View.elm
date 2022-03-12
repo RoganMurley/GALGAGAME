cfa98e56e1bb3b66c43871edbe55748f238d8d15
Rogan Murley <rogan@roganmurley.com>
2022-03-12 02:09:01 +0000

    Visuals for MIRROR GRAIL status

--- client/elm/Room/Connected/GameState/PlayState/Model/Card/View.elm
+++ client/elm/Room/Connected/GameState/PlayState/Model/Card/View.elm
@@ -67,11 +67,11 @@ view ctx entity =
                                 }
                             ]
                    )
-                ++ (List.concat <| List.map (statusView ctx entity) card.statuses)
+                ++ (List.concat <| List.indexedMap (statusView ctx entity) card.statuses)
 
 
-statusView : Context -> Card.Entity a -> Status -> List WebGL.Entity
-statusView ctx entity status =
+statusView : Context -> Card.Entity a -> Int -> Status -> List WebGL.Entity
+statusView ctx entity i status =
     let
         { camera3d, perspective, textures } =
             ctx
@@ -79,25 +79,51 @@ statusView ctx entity status =
         { position, rotation, scale, revealed } =
             entity
 
-        horizontalOffset =
+        index =
             if revealed then
-                0
+                i + 1
 
             else
-                0.5
+                i
+
+        column =
+            modBy 3 index
+
+        row =
+            index // 3
+
+        horizontalOffset =
+            0.5 * (1 - toFloat column)
+
+        verticalOffset =
+            0.85 - 0.5 * toFloat row
 
         offset =
             Matrix4.transform
                 (Quaternion.makeRotate rotation)
                 (vec3
                     (Vector3.getX scale * horizontalOffset)
-                    (Vector3.getY scale * 0.85)
+                    (Vector3.getY scale * verticalOffset)
                     0
                 )
     in
     case status of
         StatusBlighted ->
             Texture.with textures "blighted.png" <|
+                \texture ->
+                    [ Render.Primitives.quad Render.Shaders.fragment <|
+                        { rotation = Quaternion.makeRotate rotation
+                        , scale = makeScale <| Vector3.scale 0.27 scale
+                        , color = vec3 1 1 1
+                        , pos = Vector3.add position offset
+                        , perspective = perspective
+                        , camera = camera3d
+                        , texture = texture
+                        }
+                    ]
+
+        StatusEcho ->
+            Texture.with textures "multiply.png" <|
                 \texture ->
                     [ Render.Primitives.quad Render.Shaders.fragment <|
                         { rotation = Quaternion.makeRotate rotation
@@ -258,7 +284,7 @@ transmutingView ctx stackCard finalStackCard entity =
                 , time = progress
                 }
             ]
-                ++ (List.concat <| List.map (statusView ctx entity) stackCard.card.statuses)
+                ++ (List.concat <| List.indexedMap (statusView ctx entity) stackCard.card.statuses)
 
 
 backDissolvingView : Context -> Game.Entity3D a -> List WebGL.Entity
