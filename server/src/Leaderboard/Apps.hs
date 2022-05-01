723641e1a537dcbdad55f044660f0826b3605627
Rogan Murley <rogan@roganmurley.com>
2022-05-01 14:33:40 +0100

    Minor fix

--- server/src/Leaderboard/Apps.hs
+++ server/src/Leaderboard/Apps.hs
@@ -3,7 +3,7 @@ module Leaderboard.Apps where
 import Config (App, runBeam)
 import Data.Text (Text)
 import Data.String.Conversions (cs)
-import Database.Beam (all_, desc_, orderBy_, runSelectReturningList, select)
+import Database.Beam (all_, desc_, limit_, orderBy_, runSelectReturningList, select)
 import Schema (GalgagameDb (..), galgagameDb)
 import Stats.Experience (Experience)
 import qualified Stats.Schema
@@ -19,3 +19,14 @@ loadLeaderboard = do
               all_ $
                 stats galgagameDb
   return $ (\row -> (cs . show . Stats.Schema.statsUser $ row, Stats.Schema.statsExperience row)) <$> result
+
+loadLeaderboardRaw = do
+  result <-
+    runBeam $
+      runSelectReturningList $
+        select $
+          limit_ 10  $
+            orderBy_ (desc_ . Stats.Schema.statsExperience) $
+              all_ $
+                stats galgagameDb
+  return result
