c4563e5c072b535b6c4133510bc727a7f86937ad
Rogan Murley <rogan@roganmurley.com>
2022-05-07 13:15:22 +0100

    Update replay json migration

--- database/migrations/add_replay_display_usernames.sql
+++ database/migrations/add_replay_display_usernames.sql
@@ -1,2 +1,5 @@
 ALTER TABLE replays ADD COLUMN display_username_a TEXT;
 ALTER TABLE replays ADD COLUMN display_username_b TEXT;
+
+UPDATE replays SET display_username_a=json_extract_path_text(cast(replay as json), 'pa');
+UPDATE replays SET display_username_b=json_extract_path_text(cast(replay as json), 'pb');
